e116697748c9e2d66630930cad6f6845b3099316
ietix <comikfreak@gmail.com>
2019-05-13 22:06:02 -0300

    Refactoring de la solucion

---
+++ ApiLogicManager/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ApiLogicManager.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
