ac3827cf9e0090761a82ddb7b268d0292ec8699e
ietix <comikfreak@gmail.com>
2019-07-21 17:25:17 -0300

    PostAdquisition method added to start or stop Adquisition

--- AdquisitionWebApi/AdquisitionWebApi/Controllers/AdquisitionController.cs
+++ AdquisitionWebApi/AdquisitionWebApi/Controllers/AdquisitionController.cs
@@ -4,11 +4,38 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AdqLibrary;
 
 namespace AdquisitionWebApi.Controllers
 {
   public class AdquisitionController : ApiController
   {
-  public HttpResponseMessage Post()
+    /// <summary>
+    /// Starts or stops the adquisition
+    /// Route: POST api/Adquisition?start=1
+    /// </summary>
+    /// <param name="start"></param>
+    /// <returns>Http 200 Ok when the action is completed</returns>
+    public IHttpActionResult PostAdquisition([FromUri] int start)
+    {
+      try
+      {
+        if (start == 0)
+        {
+          CommunicationMgr.GetInstance().StopAdquisition();
+        }
+        else
+        {
+          CommunicationMgr.GetInstance().StartAdquisition();
+        }
+
+        return Ok();
+      }
+      catch (Exception)
+      {
+
+        throw;
+      }
+    }
   }
 }
